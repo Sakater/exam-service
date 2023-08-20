49a0fad01c525e2361a42a1c475879d8b3e83623
Yusuf Solmaz <yusuf.solmaz@th-wildau.de>
2023-08-20 20:42:23 +0200

    init

---
+++ src/test/java/exam/application/ExamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package exam.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
